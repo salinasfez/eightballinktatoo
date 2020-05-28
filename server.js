94c7583449594badc4426941264f47e60d8e1590
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-05-27 20:25:49 -0500

    all images in carousel are now same size and I have the flexbox working and responding

---
+++ server.js
@@ -0,0 +1,34 @@
+//dependencies
+const express = require('express');
+const cors = require('cors');
+const twilio = require('twilio');
+
+//twilio requirements -- texting API
+
+const accountSid = 'AC914967b1b596685c52ad515f8ad978ce';
+const authToken = 'c458588581e4a8a11cef09893cc1712c'
+const client = new twilio(accountSid, authToken);
+
+const app = express();
+//cors blocks the browser from restricting any data
+app.use(cors());
+
+//home page for server
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Express');
+})
+//twilio text
+app.get('/send-text', (req, res) => {
+    //_GET variables, passed via query string
+    const { recipient, textMessage } = req.query
+    //send text
+    client.messages.create({
+        body: textMessage,
+        // setting to number to Octavio's number
+        to: '+18159097133',
+        from: '+12025190773' // this is my  twilio number
+    }).then((message) => console.log(message.body))
+})
+
+app.listen(3000, () => console.log('App is running on port 3000'));
--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bulma/css/bulma.css';
 import Logo from '../src/images/eihgth-ball-color.png';
 import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
+import Vatos from '../src/images/vatos_locos.svg';
+
 
 
 
@@ -33,15 +35,20 @@ class App extends React.Component{
       <div className='container'>
         <div className='empty-space'>
             <img src={Logo} className='logo'/>
-            <h2>99 E Park St, Harvard, IL 60033</h2>
+            <h3>99 E Park St, Harvard, IL 60033</h3>
             
         </div>
         {/* className='body' */}
         <div className='body'>
           {/* carousel start */}
+          <div className='caroussel'>
+          
           <Carousel>
               <Carousel.Item className='carousel-item' >
                 <img  className="d-block w-100" src={require('../src/images/IMG_4946.jpeg')} alt="First slide"/>
+              </Carousel.Item>
+              <Carousel.Item className='carousel-item' >
+                <img  className="d-block w-100" src={require('../src/images/IMG_0872.jpeg')} alt="First slide"/>
               </Carousel.Item>
               <Carousel.Item >
                 <img className="d-block w-100" src={require('../src/images/IMG_0741.jpeg')} alt="Second slide"/>
@@ -89,23 +96,25 @@ class App extends React.Component{
                 <img className="d-block w-100" src={require('../src/images/IMG_5298.jpg')} alt="Second slide"/>
               </Carousel.Item>
           </Carousel>
-          <div className='carousel'>
           </div>
-          <div className='body-item'>
-             {/* form for sending text messages */}
-             <h2>Send the shop a quick message!</h2>
+          {/* form for sending text messages */}
+          <div className='messages'>
+             <h3>Send the shop a quick message!</h3>
+             <p style={{color: 'white'}}>(please make mention of your email and telephone number if you want to be reached)</p>
              <form>
              <label className='hide-info'>Your phone number: </label><br />
              <input className='hide-info' value={text.recipient} onChange={e => this.setState({ text: { ...text, recipient: e.target.value }})} /><br />
-             <div style={spacer} />
-             <label>Message</label>
-             <textarea rows={20} cols={30} placeholder={'Type your message here'}  style = {textArea} value={text.textMessage} onChange={e => this.setState({ text: { ...text, textMessage: e.target.value}})} />
+             <div />
+             <textarea rows={5} cols={39} placeholder={'Type your message here'}  style = {textArea} value={text.textMessage} onChange={e => this.setState({ text: { ...text, textMessage: e.target.value}})} />
+             <br></br>
              <Button type='submit' onClick={this.sendText}>Send text</Button>
              </form>
           </div>
-          <div className='body-item'>
-          <iframe className='body-item' src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FChicago&amp;src=c2FsaW5hc2ZlejAzMjhAZ21haWwuY29t&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=bzNnMWJlY25laThlcDJxdHZybDVkMmxndG9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%237986CB&amp;color=%2333B679&amp;color=%23C0CA33&amp;color=%230B8043" style={{border:"solid 1px #777"}} width="800" height="600" frameborder="0" scrolling="no"></iframe>
-          </div>
+          {/* start of calendar */}
+          {/* <div className='calendar'>
+            <h2>Book a free, in person, estimate!</h2>
+            <iframe  src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FChicago&amp;src=c2FsaW5hc2ZlejAzMjhAZ21haWwuY29t&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=bzNnMWJlY25laThlcDJxdHZybDVkMmxndG9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%237986CB&amp;color=%2333B679&amp;color=%23C0CA33&amp;color=%230B8043" style={{border:"solid 1px #777"}} width="800" height="600" frameborder="0" scrolling="no"></iframe>
+          </div> */}
         
         </div>
     </div>
